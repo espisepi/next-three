1f17a82228a4842e19765c6d70a22c0735293be6
espisepi <sepinaco@gmail.com>
2020-12-01 01:10:36 +0100

    horse in gatacattana

--- pages/gatacattana.js
+++ pages/gatacattana.js
@@ -11,14 +11,74 @@ import Joystick from '../drei-espinaco/Joystick';
 import Fullscreen from '../drei-espinaco/Fullscreen';
 import ShaderHorse from '../drei-espinaco/shaders/shaderHorse';
 
+function AssetGltf({ url, speed = 1 }) {
+
+    /* --------- Load horse ----------- */
+    const { nodes, materials, animations } = useLoader(GLTFLoader, url);
+
+    /* --------- Material horse ---------- */
+    const [tRoad, tLut] = useLoader(THREE.TextureLoader, [ 'assets/img/gatacattana/road.jpg', 'assets/img/gatacattana/lut.png'])
+    const { gl, scene } = useThree();
+    const canvas = gl.domElement;
+    const pixelRatio = window.devicePixelRatio;
+    const width  = canvas.clientWidth  * pixelRatio | 0;
+    const height = canvas.clientHeight * pixelRatio | 0;
+    const resolution = useMemo(()=>(new THREE.Vector2(width, height)),[width, height]) ;
+
+    const uniforms = Object.assign({}, THREE.UniformsLib.lights, {
+        iResolution: { value: resolution },
+        iChannel0: { value: scene.background },
+        iChannel1: { value: tRoad },
+        iLookup: { value: tLut },
+        opacity: { value: 1 },
+        diffuse: {  value: new THREE.Color(0xffffff) },
+		iGlobalTime: {  value: 0 },
+    });
+
+    nodes.mesh_0.material.onBeforeCompile = function (shader) {
+        shader.fragmentShader = ShaderHorse.fragmentShader;
+        Object.assign(shader.uniforms, uniforms);
+        shader.vertexShader = 'varying vec2 vUv;\n' + shader.vertexShader;
+        shader.vertexShader = 'uniform vec2 iResolution;\n' + shader.vertexShader;
+        shader.vertexShader = shader.vertexShader.replace(
+          '#include <begin_vertex>',
+          [ 
+            '#include <begin_vertex>',
+            'vec4 newPosition = projectionMatrix * modelViewMatrix * vec4(position, 1.0);',
+            'vec2 screenPos = newPosition.xy / newPosition.w;',
+            'vUv = screenPos;',
+            'vUv.x *= iResolution.x / iResolution.y;'
+          ].join('\n')
+          );
+    }
+
+
+    /* --------- Animations horse --------- */
+    const [ mixer ] = useState(() => new THREE.AnimationMixer());
+    const group = useRef();
+    useEffect(()=> void mixer.clipAction(animations[0], group.current).play(),[]);
+    useFrame((state, delta) => {
+        mixer.update(delta * speed);
+    });
+
+    return (
+        <group ref={group} dispose={null} scale={[0.2,0.2,0.2]} position={[0,-7,0]} rotation={[0,Math.PI / 2, 0]}>
+            <primitive object={nodes.mesh_0} />
+        </group>
+    );
+}
+
+
 const GatacattanaPage = () => {
   return [
     <Canvas camera={{ position: [0, 0, 35] }} style={{position: 'absolute'}}>
       <ambientLight intensity={2} />
       <Background url='assets/musica/gotham.mp4' />
       <Ocean geometry={new THREE.BoxBufferGeometry( 500, 500, 500 )} position={[0,240,0]} />
-      
       <SimondevPersonController />
+      <Suspense fallback={null}>
+            <AssetGltf url="assets/obj/Horse.glb" />
+    </Suspense>
     </Canvas>,
     <Joystick />,
     <Fullscreen />
